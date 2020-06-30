0b676723113401cdb40c0cff494d3383b66f671c
Santhosh-Gorla <30750687+Santhosh-Gorla@users.noreply.github.com>
2020-06-30 15:37:38 +0530

    Add files via upload

---
+++ pmu.js
@@ -0,0 +1,33 @@
+
+
+
+
+	$(function(){
+	   $("#mySelect").change(function(){
+	   var displaycourse=$("#mySelect option:selected").val(); 
+	   $("#txtresults").val(displaycourse);
+	   })
+	 })
+	  $(document).ready(function() {
+    $("button").click(function(){
+	 var selected = $('#mySelect').val();
+	   $("#txtresults").val(selected);
+
+    });
+});
+	  $(document).ready(function() {
+     $('#put').click(function() {
+      var selected = $('#mySelect').val();
+	   $("#txtresults").val(selected);
+     
+  })
+});
+jQuery(document).ready(function(){
+	jQuery(".chosen").chosen();
+	
+	var pmuname = document.getElementById("mySelect").value;
+  
+});
+		function getOption() {  
+		var pmuname = document.getElementById("mySelect").value;
+		} 
